a9765e4daa6026517d5f30a13ea6d337f0943b7a
jintae-1224 <kab7210@naver.com>
2023-05-10 22:41:29 +0900

    onError

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -13,7 +13,15 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
+  const toast = useCustomToast();
+
   const fallback = [];
-  const { data = fallback } = useQuery([queryKeys.treatments], getTreatments);
+  const { data = fallback } = useQuery([queryKeys.treatments], getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error ? error.message : 'error connecting of Server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
